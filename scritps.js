a5b59c9aa57e25960b33e873d12b576f26be5444
Yuri Padua <yuri.padua@prof.uniso.br>
2023-03-25 05:38:24 -0300

    pagina funcionando

---
+++ scritps.js
@@ -0,0 +1,38 @@
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", function() {
+  const name = prompt("Qual é o seu nome?");
+  const operation = prompt("Quais operações matemáticas você deseja fazer? (+, -, * ou /)");
+
+  let result;
+  let num1;
+  let num2;
+
+  do {
+    num1 = Number(prompt("Digite o primeiro número:"));
+    num2 = Number(prompt("Digite o segundo número:"));
+
+    switch (operation) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      case "/":
+        result = num1 / num2;
+        break;
+      default:
+        alert("Operação inválida. Por favor, escolha uma operação válida (+, -, * ou /).");
+    }
+  } while (isNaN(result));
+
+  alert("O resultado da operação " + num1 + " " + operation + " " + num2 + " é " + result + ".");
+
+  const newDiv = document.createElement("div");
+  newDiv.innerHTML = "<h2>Olá, " + name + "!</h2><p>O resultado da operação " + num1 + " " + operation + " " + num2 + " é " + result + ".</p>";
+  document.body.appendChild(newDiv);
+});
