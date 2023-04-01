83fe6d64c0221e6532c81cbfc376da22601bdad3
ProfYuri <profyuri@uniso.br>
2023-04-01 08:41:47 -0300

    editando jse css no local

--- scritps.js
+++ scritps.js
@@ -1,38 +1,94 @@
-const myButton = document.getElementById("myButton");
+const display = document.querySelector(".display");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equal = document.querySelector(".equal");
+const clear = document.querySelector(".clear");
+const decimal = document.querySelector(".decimal");
 
-myButton.addEventListener("click", function() {
-  const name = prompt("Qual é o seu nome?");
-  const operation = prompt("Quais operações matemáticas você deseja fazer? (+, -, * ou /)");
+let firstOperand = null;
+let secondOperand = null;
+let operator = null;
+let shouldReset = false;
 
+function updateDisplay(value) {
+  if (shouldReset) {
+    display.textContent = "";
+    shouldReset = false;
+  }
+  display.textContent += value;
+}
+
+function reset() {
+  display.textContent = "";
+  firstOperand = null;
+  secondOperand = null;
+  operator = null;
+}
+
+function calculate() {
   let result;
-  let num1;
-  let num2;
+  const a = parseFloat(firstOperand);
+  const b = parseFloat(secondOperand);
 
-  do {
-    num1 = Number(prompt("Digite o primeiro número:"));
-    num2 = Number(prompt("Digite o segundo número:"));
+  switch (operator) {
+    case "+":
+      result = a + b;
+      break;
+    case "-":
+      result = a - b;
+      break;
+    case "*":
+      result = a * b;
+      break;
+    case "/":
+      result = a / b;
+      break;
+    default:
+      return;
+  }
 
-    switch (operation) {
-      case "+":
-        result = num1 + num2;
-        break;
-      case "-":
-        result = num1 - num2;
-        break;
-      case "*":
-        result = num1 * num2;
-        break;
-      case "/":
-        result = num1 / num2;
-        break;
-      default:
-        alert("Operação inválida. Por favor, escolha uma operação válida (+, -, * ou /).");
+  display.textContent = result;
+  firstOperand = result;
+  secondOperand = null;
+  operator = null;
+  shouldReset = true;
+}
+
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    updateDisplay(number.value);
+  });
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    if (firstOperand === null) {
+      firstOperand = display.textContent;
+      operator = operator.value;
+      shouldReset = true;
+    } else if (operator !== null) {
+      secondOperand = display.textContent;
+      calculate();
+      operator = operator.value;
+    } else {
+      operator = operator.value;
+      shouldReset = true;
     }
-  } while (isNaN(result));
+  });
+});
 
-  alert("O resultado da operação " + num1 + " " + operation + " " + num2 + " é " + result + ".");
+equal.addEventListener("click", () => {
+  if (secondOperand !== null) {
+    calculate();
+  }
+});
 
-  const newDiv = document.createElement("div");
-  newDiv.innerHTML = "<h2>Olá, " + name + "!</h2><p>O resultado da operação " + num1 + " " + operation + " " + num2 + " é " + result + ".</p>";
-  document.body.appendChild(newDiv);
+clear.addEventListener("click", () => {
+  reset();
+});
+
+decimal.addEventListener("click", () => {
+  if (!display.textContent.includes(".")) {
+    updateDisplay(decimal.value);
+  }
 });
